318ccff4d15f5280528666abe456af6e18621acc
Srijan Dahal <srijan102dahal@gmail.com>
2025-09-29 00:04:45 +0545

    fixed: space grotesk font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { orbitronRegular, pixelifySansRegular } from "./utils/fonts";
+import { orbitronRegular, pixelifySansRegular, spaceGroteskRegular } from "./utils/fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${orbitronRegular.variable} ${pixelifySansRegular.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${orbitronRegular.variable} ${pixelifySansRegular.variable} ${spaceGroteskRegular.variable} antialiased`}
       >
         {children}
       </body>

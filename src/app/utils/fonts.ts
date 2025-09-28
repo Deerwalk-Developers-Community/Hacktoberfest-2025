16165474627cb1ea83dd77f65cb151789f6e1354
Srijan Dahal <srijan102dahal@gmail.com>
2025-09-28 23:08:34 +0545

    added: fonts locally

---
+++ src/app/utils/fonts.ts
@@ -0,0 +1,64 @@
+import localFont from "next/font/local";
+
+export const orbitronRegular = localFont({
+  src: "../assets/fonts/orbitron/Orbitron-Regular.woff2",
+  variable: "--font-orbitron-regular",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const orbitronMedium = localFont({
+  src: "../assets/fonts/orbitron/Orbitron-Medium.woff2",
+  variable: "--font-orbitron-medium",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const orbitronBold = localFont({
+  src: "../assets/fonts/orbitron/Orbitron-Bold.woff2",
+  variable: "--font-orbitron-bold",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const orbitronExtraBold = localFont({
+  src: "../assets/fonts/orbitron/Orbitron-ExtraBold.woff2",
+  variable: "--font-orbitron-extrabold",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const orbitronBlack = localFont({
+  src: "../assets/fonts/orbitron/Orbitron-Black.woff2",
+  variable: "--font-orbitron-black",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const pixelifySansRegular = localFont({
+  src: "../assets/fonts/pixelify_sans/PixelifySans-Regular.woff2",
+  variable: "--font-pixelifysans-regular",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const pixelifySansMedium = localFont({
+  src: "../assets/fonts/pixelify_sans/PixelifySans-Medium.woff2",
+  variable: "--font-pixelifysans-medium",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const pixelifySansSemiBold = localFont({
+  src: "../assets/fonts/pixelify_sans/PixelifySans-SemiBold.woff2",
+  variable: "--font-pixelifysans-semibold",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
+
+export const pixelifySansBold = localFont({
+  src: "../assets/fonts/pixelify_sans/PixelifySans-Bold.woff2",
+  variable: "--font-pixelifysans-bold",
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
